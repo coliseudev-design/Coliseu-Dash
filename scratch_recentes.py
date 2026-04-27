0ff11d52af1e953f02be534798565ff45bf848a8
Coliseu Sistemas <coliseu.dev@gmail.com>
2026-04-27 17:01:07 -0400

    fix(ui): trim status string to prevent empty recent orders list

--- scratch_recentes.py
+++ scratch_recentes.py
@@ -2,6 +2,6 @@ import paramiko
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('177.39.17.7', username='root', password='6EFBC!c0:wzr%Ij')
-script = "docker exec coliseu-db-thyqkc5gkvp7i1nld555wakz-131845936540 psql -U coliseu_admin -d coliseu_dashboard -c 'SELECT id_firebird, status, data_venda FROM dash_vendas ORDER BY data_venda DESC LIMIT 15;'"
+script = "docker exec coliseu-db-thyqkc5gkvp7i1nld555wakz-131845936540 psql -U coliseu_admin -d coliseu_dashboard -c 'SELECT LENGTH(status), status FROM dash_vendas LIMIT 5;'"
 stdin, stdout, stderr = client.exec_command(script)
 print(stdout.read().decode('utf-8'))
