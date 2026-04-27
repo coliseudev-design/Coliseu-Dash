dac53a8fdcbbc81de686c51faa27c79bbe89188e
Coliseu Sistemas <coliseu.dev@gmail.com>
2026-04-27 13:12:08 -0400

    atualizar

---
+++ get_docker_logs.py
@@ -0,0 +1,10 @@
+import paramiko
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect('177.39.17.7', username='root', password='6EFBC!c0:wzr%Ij')
+
+script = "docker logs coliseu-middleware-xkgco0sskgwwcwsogsk8sog4 --tail 100"
+stdin, stdout, stderr = client.exec_command(script)
+print('STDOUT:', stdout.read().decode('utf-8'))
+print('STDERR:', stderr.read().decode('utf-8'))
