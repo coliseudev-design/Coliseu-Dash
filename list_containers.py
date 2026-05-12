3d414020bc83dfc7bc39057bdb59ea92ee21788e
Coliseu Sistemas <coliseu.dev@gmail.com>
2026-05-12 10:48:17 -0400

    ate

--- list_containers.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('177.39.17.7', username='root', password='6EFBC!c0:wzr%Ij')
-
-script = "docker ps --format '{{.Names}}'"
-stdin, stdout, stderr = client.exec_command(script)
-print('STDOUT:', stdout.read().decode('utf-8'))
